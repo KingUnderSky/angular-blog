61ee0fbb6c245b71de008bdb739e950c803abf65
Ettore A. Rollo <ettorerollo@hotmail.com>
2022-09-06 11:48:47 -0300

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TitleBarComponent } from './components/title-bar/title-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleBarComponent,
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
